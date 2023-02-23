3edcae89c4894deabc613df05b51cf269223b1dd
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-02-22 19:54:51 -0500

    Update keywordRank.py
    added equation

--- keywordRank.py
+++ keywordRank.py
@@ -12,8 +12,8 @@ query = "SELECT * FROM tweets WHERE " + " AND ".join(["text LIKE ?" for _ in key
 c.execute(query, tuple(f'%{keyword}%' for keyword in keywords))
 tweets = c.fetchall()
 
-# Process the tweets
-scores = {}
+# Compute the score of each tweet
+scores = []
 for tweet in tweets:
     # Extract relevant information from the tweet
     text = tweet[1]
@@ -22,26 +22,24 @@ for tweet in tweets:
     verified = tweet[4]
     promoted = tweet[5]
 
-    # Score the tweet based on its relevance to the keywords and other factors
-    score = 0
-    if all(keyword in text for keyword in keywords):
-        score = len(keywords)
-        if verified:
-            score += 1
-        if promoted:
-            score += 2
+    # Compute the score of the tweet
+    K = sum(keyword in text for keyword in keywords)
+    m = 1 if K == len(keywords) else 0
+    V = 1 if verified else 0
+    P = 2 if promoted else 0
+    score = K * m + V + P
 
-    # Add the tweet and its score to the scores dictionary
-    scores[tweet] = score
+    # Add the tweet and its score to the scores list
+    scores.append((tweet, score))
 
-# Sort the tweets by their score, with promoted tweets first
-sorted_tweets = sorted(scores.items(), key=lambda x: (-x[1], -x[0][3], -x[0][0]))
+# Sort the tweets by their score in descending order
+sorted_tweets = sorted(scores, key=lambda x: -x[1])
 
 # Display the tweets and their scores
 for tweet, score in sorted_tweets:
     text = tweet[1]
     author = tweet[2]
     date = tweet[3]
-    verified = tweet[4]
     promoted = tweet[5]
     print(f'{text} ({author}, {date}){" [PROMOTED]" if promoted else ""}: {score}')
+
